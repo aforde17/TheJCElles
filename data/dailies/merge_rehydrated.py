e30a133dbe9f8ee5969e1fca07eef2d38d363242
Akila Forde <aforde@linux1.cs.uchicago.edu>
2022-05-23 15:52:42 -0500

    Code to merge all individual json files into one dataset to work off

---
+++ data/dailies/merge_rehydrated.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pathlib import Path
+from datetime import date
+
+home_path = Path()
+tweets_path = home_path.joinpath("rehydrated")
+
+# looping over all files in the directory
+files = Path(tweets_path).glob('*')
+
+list_of_dfs = []
+for file in files:
+    df = pd.read_json(file)
+    # Pulling out date rather than datetime
+    df['date'] = pd.to_datetime(df['created_at']).dt.date
+    df = df[['date', 'text']]
+    list_of_dfs.append(df)
+
+# Concatnating the list of subsetted dfs
+full_df = pd.concat(list_of_dfs)
+
+full_df.to_csv("full_tweets.csv")
