bddffe5784f0281fcc89fc49d45801b4df5e7ab6
Idalina Sachango <idalinassachango@MacBook-Pro-4.attlocal.net>
2022-06-01 21:15:41 -0500

    Abort

--- analysis/k-means.py
+++ analysis/k-means.py
@@ -8,7 +8,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+import nltk
+import nltk
+from collections import Counter
+nltk.download('words')
 
+english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+keywords = ["mask", "wearamask", "masking", 
+            "masked", "unmask", "unmasked",
+            "unmasking", "anti-mask", "maskon", 
+            "maskoff", "N95", "face cover", "face covering", 
+            "face covered", "mouth cover", "mouth covering", 
+            "mouth covered", "nose cover", "nose covering", "nose covered", 
+            "cloth covering", "cover your face", "coveryourface", "facemask", 
+            "face diaper", "n95", "n-95", "kn95", "kn-95", "respirator","covid19", "wear", "people", "face", "do",
+            "coronavirus", "covid", "sars"]
 
 home_path = Path(__file__).parent.parent
 data_path = home_path.joinpath("data/dailies/")
@@ -16,76 +30,111 @@ fig_path = home_path.joinpath("plots/")
 cleaning = home_path.joinpath("cleaning/")
 
 
+def elbow_method(Y_sklearn):
+    """
+    This is the function used to get optimal number of clusters in order to feed to the k-means clustering algorithm.
+    """
+
+    number_clusters = range(1, 7)  # Range of possible clusters that can be generated
+    kmeans = [KMeans(n_clusters=i, max_iter = 600) for i in number_clusters] # Getting no. of clusters 
+
+    score = [kmeans[i].fit(Y_sklearn).score(Y_sklearn) for i in range(len(kmeans))] # Getting score corresponding to each cluster.
+    score = [i*-1 for i in score] # Getting list of positive scores.
+    
+    plt.plot(number_clusters, score)
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Score')
+    plt.title('Elbow Method')
+    plt.show()
 # tweets = pd.DataFrame(pd.read_json(data_path.joinpath("rehydrated-2022-01-13.json")))
 
 df = pd.read_pickle(data_path.joinpath("full_tweets_clean.pkl"))
 
 df = pd.DataFrame(df)
 df['text'] = df['text'].str.join(" ")
-df = df[["date", "text"]]
 
 
-keyword = ["mask", "wearamask", "masking", "N95", "face cover", "face covering", "face covered", "mouth cover", "mouth covering",
-"mouth covered", "nose cover", "nose covering", "nose covered", "cover your face", "coveryourface"]
 
 
-filtered_tweets = []
-
-for tweet in df.itertuples():
-    for word in keyword:
-        if word in tweet.text:
-            filtered_tweets.append(tweet.text)
-        break
-print("Done filtering")
-
-
+df_copy = df.copy()
 print("Making tf-idf vector")
 tf_idf_vectorizor = TfidfVectorizer(stop_words = 'english')
 print("Initializing dimensionality reduction")
 truncate = TruncatedSVD()
-
 print("Making features")
-features = tf_idf_vectorizor.fit_transform(filtered_tweets)
+features = tf_idf_vectorizor.fit_transform(df_copy['text'])
 print("Done making features")
-
-
-
 print("Initializing kmeans")
 model = KMeans(n_clusters=3, init='k-means++', max_iter=100, n_init=1)
-
 # # was using scaler.fit_transform(scale(features.T)) as input for Y_sklearn
-print("Reducing dimensions for tf-idf vector")
+# print("Reducing dimensions for tf-idf vector")
 Y_sklearn = truncate.fit_transform(features)
+elbow_method(Y_sklearn)
 
+# print("Fitting reduced tf-idf")
+# fit = model.fit(features)
+# print("Predicting clusters from tf-idf")
+# predict = model.predict(features)
+# df_copy['cluster'] = model.labels_
+# df_copy['text'] = df_copy['text'].str.split()
 
-# word_positions = {v: k for k, v in tf_idf_vectorizor.vocabulary_.items()}
-# print(word_positions)
+# # for n = 3
+# d1 = {}
+# d2 = {}
+# d3 = {}
+# for row in df_copy[['text', 'cluster']].itertuples():
+#     for word in row.text:
+#         if word not in keywords:
+#             if row.cluster == 0:
+#                 if word not in d2.keys() and word not in d3.keys():
+#                     if word in english_vocab:
+#                         d1[word] = d1.get(word, 0) + 1
+#             if row.cluster == 1:
+#                 if word not in d1.keys() and word not in d3.keys():
+#                     if word in english_vocab:
+#                         d2[word] = d2.get(word, 0) + 1
+#             if row.cluster == 2:
+#                 if word not in d1.keys() and word not in d2.keys():
+#                     if word in english_vocab:
+#                         d3[word] = d3.get(word, 0) + 1
+# common_cluster_one = Counter(d1).most_common(20)
+# common_cluster_two = Counter(d2).most_common(20)
+# common_cluster_three = Counter(d3).most_common(20)
+# for idx, (word, _) in enumerate(common_cluster_one):
+#     common_cluster_one[idx] = word
+# for idx, (word, _) in enumerate(common_cluster_two):
+#     common_cluster_two[idx] = word
+# for idx, (word, _) in enumerate(common_cluster_three):
+#     common_cluster_three[idx] = word
 
-print("Fitting reduced tf-idf")
-fitted = model.fit(Y_sklearn)
-print("Predicting clusters from tf-idf")
-predict = model.predict(Y_sklearn)
+# def models(n_clusters, df):
+#     copy = df.copy()
+#     d = {}
+#     for row in df_copy[['text', 'cluster']].itertuples():
+#         for word in row.text:
+#             if word not in keywords:
+#                 d[row.cluster] = d.get(row.cluster, {})
+#                 lst = [y for x, y in d.items() if x != row.cluster]
+#                 d[row.cluster][word] = d[row.cluster].get(word, 0) + 1
 
-# testing 
-# # dist_words = sorted(v for k, v in word_positions.items())
-# # print(dist_words)
-# for cluster in set(predict):
-#     tfidf = features[predict == cluster]
-#     tfidf[tfidf > 0] = 1
-
-#     df = pd.DataFrame(tfidf)
-#     print(df)
-
-print(model.labels_)
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.scatter(Y_sklearn[:, 0], Y_sklearn[:, 1],c=predict ,s=50, cmap='viridis') # Plotting scatter plot 
-ax.legend(predict)
-
-centers2 = fitted.cluster_centers_ # It will give best possible coordinates of cluster center after fitting k-means
-
-ax.scatter(centers2[:, 0], centers2[:, 1],c='black', s=300, alpha=0.6)
-plt.savefig(fig_path.joinpath("cluster-unscaled.png"))
-
-
-
-
+#                 if row.cluster == 0:
+#                     if word not in d2.keys() and word not in d3.keys():
+#                         if word in english_vocab:
+#                             d1[word] = d1.get(word, 0) + 1
+#                 if row.cluster == 1:
+#                     if word not in d1.keys() and word not in d3.keys():
+#                         if word in english_vocab:
+#                             d2[word] = d2.get(word, 0) + 1
+#                 if row.cluster == 2:
+#                     if word not in d1.keys() and word not in d2.keys():
+#                         if word in english_vocab:
+#                             d3[word] = d3.get(word, 0) + 1
+#     common_cluster_one = Counter(d1).most_common(20)
+#     common_cluster_two = Counter(d2).most_common(20)
+#     common_cluster_three = Counter(d3).most_common(20)
+#     for idx, (word, _) in enumerate(common_cluster_one):
+#         common_cluster_one[idx] = word
+#     for idx, (word, _) in enumerate(common_cluster_two):
+#         common_cluster_two[idx] = word
+#     for idx, (word, _) in enumerate(common_cluster_three):
+#         common_cluster_three[idx] = word
